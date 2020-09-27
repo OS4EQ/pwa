1be673146620117d29b1c5d296df8987f299a12e
paulazoo <paulazhu@college.harvard.edu>
2020-09-27 15:11:10 -0400

    Update gatsby and react also setup example graphql query

--- src/pages/testing.js
+++ src/pages/testing.js
@@ -3,16 +3,29 @@ import React from 'react'
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const Testing = () => {
+import { graphql } from 'gatsby'
+
+const Testing = ({ data }) => {
   return (
     <Layout>
       <SEO title="Testing" />
       <div>
         <h1>Testing Page</h1>
         <p>hello!! my name is paula zhu yeah it is and i like pie🚀</p>
+        <b>{'heres a test graphql query from the config file: '+data.site.siteMetadata.description}</b>
       </div>
     </Layout>
   )
 }
 
 export default Testing
+
+export const query = graphql`
+  query TestingQuery {
+    site {
+      siteMetadata {
+        description
+      }
+    }
+  }
+`
