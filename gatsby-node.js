f724c3834a2bda8e26961437b9f923a27d1ba3fd
Tulsi Patel <tulsipatel@Tulsis-MBP.fios-router.home>
2020-12-05 06:36:44 -0500

    new queries added

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,3 +5,31 @@
  */
 
 // You can delete this file if you're not using it
+
+// posts.forEach(({ node }, index) => {
+//     createPage({
+//       path: node.fields.slug,
+//       component: path.resolve(`./src/pages/index.js`),
+//       // values in the context object are passed in as variables to page queries
+//       context: {
+//         published: node.published, // "Using a Theme"
+//       },
+//     })
+//   })
+
+// createPage({
+//     path: `/`,
+//     component: path.resolve(
+//         `./src/pages/index.js`,
+//     ),
+//     context: {
+//         before: ``, 
+//         after: ``, 
+//         first: ``, 
+//         last: ``, 
+//         id: ``, 
+//         name: ``, 
+//         audioUrl: ``, 
+//         published: `True`,
+//     },
+// });
